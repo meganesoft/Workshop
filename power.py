2600bc80100e7624de4b3df241c62c6cbbc9546b
meganesoft <meganesoft87@gmail.com>
2018-12-13 21:37:51 +0900

    4進数

---
+++ power.py
@@ -0,0 +1,13 @@
+N = int(input())
+
+total = sum([pow(p,N) for p in range(1,5) ])
+
+print(total)
+
+def Base_four(X,n):
+    if (int(X/n)):
+        return Base_four(int(X/n),n)+str(X%n)
+    return str(X%n)
+
+x4 = Base_four(total,4)
+print(x4)
