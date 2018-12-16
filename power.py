494ea77f1a34041333a4cef2cd0acbe2c441096d
meganesoft <meganesoft87@gmail.com>
2018-12-17 01:01:23 +0900

    計算速度が足りない

--- power.py
+++ power.py
@@ -1,5 +1,7 @@
-N = int(input())
+from collections import Counter
+import re
 
+N = int(input())
 total = max([pow(p,N) for p in range(1,5)])
 
 print(total)
@@ -9,5 +11,13 @@ def Base_four(X,n):
         return Base_four(int(X/n),n)+str(X%n)
     return str(X%n)
 
-x4 = Base_four(total,4)
-print(x4)
+def count_three(x: str) -> int:
+    cnt = 0
+    for word in re.finditer('3',x):
+        cnt += 1
+    return cnt
+
+x4 = [str(Base_four(t,4)) for t in range(total)]
+x4= ','.join(x4)
+print(count_three(x4))
+#print(count_three(x4))
